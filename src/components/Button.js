abdb281dec99fb73dba1df7c3b1caba067d445ac
Lily Tan <lilyt93@gmail.com>
2021-06-30 13:40:49 -0400

    added countapi

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,12 +1,32 @@
 import React from 'react';
+import countapi from 'countapi-js';
 
 export default class Button extends React.Component {
-
-    render() {
-        
-
-        return (
-            <button>click me</button>
-        )
+  constructor() {
+    super() 
+    this.state = {
+        hits:0
     }
+
+    this.showHits = this.showHits.bind(this)
+  }
+  
+   showHits() {
+    countapi.hit('1ccb732e-b55a-4404-ad3f-0f99c02fe44e').then((result) => {
+      this.setState({
+          hits: result.value
+      })
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <button onClick = {this.showHits}>Show Number of Hits</button>
+      <div>
+        <h1>{this.state.hits}</h1>
+      </div>
+      </div>
+    )
+  } 
 }
